e854e125bebc7a765944177249d9f235c9708abf
Vitor Valerio Hoffmann <vitorvaleriohoffmann@gmail.com>
2024-06-03 20:50:17 -0300

    feat: scroll interno suave

--- script.js
+++ script.js
@@ -40,3 +40,22 @@ function initTabNav() {
     }
   }
   initAccordion();
+  
+  function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  
+    function scrollToSection(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute('href');
+      const section = document.querySelector(href);
+      section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  
+    linksInternos.forEach((link) => {
+      link.addEventListener('click', scrollToSection);
+    });
+  }
+  initScrollSuave();
