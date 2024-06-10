351d7d6805caa4d2ed443b57b1255cd0d72325b6
Vitor Valerio Hoffmann <vitorvaleriohoffmann@gmail.com>
2024-06-09 21:24:56 -0300

    feat: adicionado menu dropdown

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -2,6 +2,38 @@ export default function initDropdownMenu() {
 
     
 
+}
 
+const dropdownMenus = document.querySelectorAll('[data-dropdown]')
+dropdownMenus.forEach(menu => {
+    ['touchstart', 'click'].forEach(userEvent => {
+        menu.addEventListener(userEvent, handleClick)
+    })
+})
 
+function handleClick(event){
+    event.preventDefault()
+    this.classList.add('active')
+    outsideClick(this, ['touchstart', 'click'], () => {
+        this.classList.remove('active')
+    })
+}
+function outsideClick(element, events, callback){
+    const html = document.documentElement
+    const outside = 'data-outside'
+    if(!element.hasAttribute(outside)){
+        events.forEach(userEvent => {
+            html.addEventListener(userEvent, handleOutsideClick)
+        })
+        element.setAttribute(outside, '')
+    }
+    function handleOutsideClick(event){
+        if (!element.contains(event.target)){
+            callback()
+            events.forEach(userEvent => {
+                html.removeEventListener(userEvent, handleOutsideClick)
+            })
+            element.removeAttribute(outside, '')
+        }
+    }
 }
